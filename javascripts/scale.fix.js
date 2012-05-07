8b9471b03ae46d5c24d18fa538f6600d0e7748f0
Nils Maier <maierman@web.de>
2012-05-07 19:33:15 +0200

    Iphone stuff sucks

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,17 +1,16 @@
-var metas = document.getElementsByTagName('meta');
-var i;
+"use strict";
 if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
+  var metas = document.getElementsByTagName('meta');
+  for (var i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
       metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
     }
   }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+  document.addEventListener("gesturestart", function gestureStart() {
+    for (var i=0; i<metas.length; i++) {
+      if (metas[i].name == "viewport") {
+        metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+      }
     }
-  }
+  }, false);
 }
